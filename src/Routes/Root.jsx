420b89d64e2e63df8e28579b4f8243ddcc934732
danismz17 <128304957+danismz17@users.noreply.github.com>
2023-06-02 16:06:51 -0300

    Add files via upload

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -2,12 +2,14 @@ import './../Styles/App.css'
 import Header from '../Components/A-Header/Header'
 import Dashboard from '../Components/B-Dashboard/Dashboard'
 import MovileControls from '../Components/C-Subcomponentes/MovileControls'
+import IntroAnimation from '../Components/C-Subcomponentes/IntroAnimation'
 
 function Root() {
 
   return (
     <>
       <div className="web-container">
+        <IntroAnimation/>
         <Header/>
         <Dashboard/>
       </div>
